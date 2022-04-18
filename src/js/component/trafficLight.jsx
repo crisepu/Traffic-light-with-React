8e113e4ebda5b9c0483ab0444abc20735880930d
crisepu <csepulvedavenegas@gmail.com>
2022-04-18 19:26:07 +0000

    init exercise

---
+++ src/js/component/trafficLight.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const TrafficLight = () => {
+	return (
+		<>
+			<div className="topTrafficLight"></div>
+			<div className="trafficLight">
+				<div className="redLight"></div>
+				<div className="yellowLight"></div>
+				<div className="greenLight"></div>
+			</div>
+		</>
+
+	)
+};
+
+export default TrafficLight;
